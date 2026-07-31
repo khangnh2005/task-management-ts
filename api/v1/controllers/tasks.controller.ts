323244fa6d1f0385cb4113eaebd7528931cf2b78
Khang Nguyễn <dh52300789@student.stu.edu.vn>
2026-07-31 19:48:18 +0700

    Bo loc trang thai

--- api/v1/controllers/tasks.controller.ts
+++ api/v1/controllers/tasks.controller.ts
@@ -1,7 +1,17 @@
 import Task from "../models/task.model";
 import { Request , Response} from"express";
 export const index = async (req: Request, res: Response) => {
-  const tasks = await Task.find({deleted : false})
+  interface Find {
+    deleted : boolean,
+    status? : string
+  }
+  const find : Find = {
+    deleted : false 
+  }
+  if(req.query.status){
+    find.status  = req.query.status.toString()
+  }
+  const tasks = await Task.find(find)
   res.json(tasks)
 }
 
